6c8b06913669312014fe3ee17466583bfefca0e2
tani <tani@1-10.com>
2021-02-24 09:07:35 +0900

    Update useContext

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,24 @@
 import Head from 'next/head'
 import styles from '../styles/Home.module.css'
-import React, { useState } from 'react'
+import React, { useState, useEffect, createContext, useContext } from 'react'
+
+const Context = createContext()
+
+const Mago = () => {
+  const { money } = useContext(Context)
+  return <p>{money}円</p>
+}
+
+const Kodomo = () => <Mago />
 
 const Home = () => {
   const [count, setCount] = useState(0)
   const [name, setName] = useState('')
+
+  useEffect(() => {
+    // stateの変更などの再レンダリングの度に実行
+    console.log('hello useEffect')
+  }, [count])
 
   return (
     <div>
@@ -12,6 +26,10 @@ const Home = () => {
         <title>Create Next App</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
+
+      <Context.Provider value={{ money: 1000 }}>
+        <Kodomo />
+      </Context.Provider>
 
       <main>
         <section>
